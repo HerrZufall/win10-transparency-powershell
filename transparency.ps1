986efb63d64a0f8e6ba7fe71fbdd8f7203824005
Thomas Friedrich Böhme <38049567+HerrZufall@users.noreply.github.com>
2021-04-27 16:58:53 +0200

    Explorer Windows

--- transparency.ps1
+++ transparency.ps1
@@ -1,17 +1,11 @@
-Write-Host "
-  
-    Make Windows transparent
-    :( Does not work on Explorer-Windows so far... 
-    :( and Admin-Windows for sure... XD
-    
-  "
+# Sets Transparency for Windows 10 through Powershell
 
 #### Settings
 # Opacity value between 0 and 255 - 0 will be fully transparent and unusable. DON'T!
-$opacity = 240
+$opacity = 230
 # Transparency will be set in a loop every ... seconds.
 $StepsInSeconds = 1
-# These will be set to not transparent.
+# These will not be transparent.
 $notTransparentWindowNames = "youtube|you tube|netflix|vimeo|vlc|video"
 
 function Set-Transparency {
@@ -24,15 +18,42 @@ function Set-Transparency {
         $opacity
   )
   PROCESS {
-    Write-Host [ $_.ProcessName ] $_.MainWindowTitle
+    Write-Host [ $process.ProcessName ] $process.MainWindowTitle
     $windowLong = $user32::GetWindowLong($process.MainWindowHandle, -20)
-    $user32::SetWindowLong($_.MainWindowHandle, -20, ($windowLong -bor 0x80000)) | Out-Null
+    $user32::SetWindowLong($process.MainWindowHandle, -20, ($windowLong -bor 0x80000)) | Out-Null
     $user32::SetLayeredWindowAttributes($process.MainWindowHandle, 0, $opacity, 0x02) | Out-Null
   }
 }
 
+function Set-ExplorerTransparency {
+  [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,
+                   ValueFromPipeline)]
+        $process,
+        $user32,
+        $opacity
+  )
+  PROCESS {
+    Write-Host [ $_.Name ] $_.LocationName
+    $windowLong = $user32::GetWindowLong($_.HWND, -20)
+    $user32::SetWindowLong($_.HWND, -20, ($windowLong -bor 0x80000)) | Out-Null
+    $user32::SetLayeredWindowAttributes($_.HWND, 0, $opacity, 0x02) | Out-Null
+  }
+}
+
+
 
 While ('yes' -eq 'yes') { 
+
+  clear
+  Write-Host "
+  
+    Transparency for Windows 10
+    (c) Thomas Friedrich Boehme
+
+  "
+
  
 $user32 = Add-Type -Name 'user32' -Namespace 'Win32' -PassThru -MemberDefinition @'
 
@@ -51,11 +72,13 @@ NOT TRANSPARENT [$notTransparentWindowNames]
 ---------------"
 Get-Process | Where-Object {$_.MainWindowTitle -match $notTransparentWindowNames } | Set-Transparency -user32 $user32 -opacity 255
 
-
 Write-Host "
 TRANSPARENT
 -----------"
 Get-Process | Where-Object {-not ([string]::IsNullOrEmpty($_.MainWindowTitle)) -and ($_.MainWindowTitle -notMatch $notTransparentWindowNames)} | Set-Transparency -user32 $user32 -opacity $opacity
+#ExplorerWindows
+$app = New-Object -COM 'Shell.Application'
+$app.Windows() | Set-ExplorerTransparency -user32 $user32 -opacity $opacity
 
 Write-Host "
 
